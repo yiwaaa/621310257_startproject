520e7ab3784b4554ee31203fed0fe7cdd02fa7a6
Phurida Kheawthong <kheawthong_p@silpakorn.edu>
2021-09-19 22:36:22 +0700

    add

---
+++ Chat Room_Client/Assets/Scritps/Model/BaseModel.cs
@@ -0,0 +1,5 @@
+public class BaseModel
+{
+    public string ClassName { get; set; }
+    protected BaseModel(string className) => ClassName = className;
+}
---
+++ Chat Room_Client/Assets/Scritps/Model/MessageModel.cs
@@ -0,0 +1,8 @@
+public class MessageModel : BaseModel
+{
+    public static readonly string CLASS_NAME = typeof(MessageModel).Name;
+    public MessageModel() : base(CLASS_NAME) { }
+    public string sender { get; set; }
+    public string message { get; set; }
+
+}
